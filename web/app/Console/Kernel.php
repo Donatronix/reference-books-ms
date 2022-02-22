ca62c6edb1346eeb57476c1f45bb8ffc7ecc44d1
Ihor Porokhnenko <ihor.porokhnenko@gmail.com>
2022-02-22 14:26:54 +0200

    add sumra sdk

--- web/app/Console/Kernel.php
+++ web/app/Console/Kernel.php
@@ -3,8 +3,6 @@
 namespace App\Console;
 
 use App\Console\Commands\GetCurrencies;
-use App\Console\Commands\KeyGenerateCommand;
-use App\Console\Commands\RouteListCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Laravel\Lumen\Console\Kernel as ConsoleKernel;
 
@@ -16,8 +14,6 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        KeyGenerateCommand::class,
-        RouteListCommand::class,
         GetCurrencies::class
     ];
 

b5a503f2fd4a0cbb59b83be0b0ee6c19062606d1
Samson Ajax <nacosseruib@gmail.com>
2022-06-25 17:01:49 +0100

    Refactor Coin Market Cap codes

--- web/app/Api/V1/Controllers/CoinMarketCapController.php
+++ web/app/Api/V1/Controllers/CoinMarketCapController.php
@@ -3,7 +3,7 @@
 namespace App\Api\V1\Controllers;
 
 use App\Http\Controllers\Controller;
-use App\Services\CurrencyExchange\CurrencyExchange;
+use App\Api\V1\Controllers\CurrencyExchange;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use ReflectionException;
@@ -18,12 +18,22 @@ class CoinMarketCapController extends Controller
      */
     public function index(Request $request): JsonResponse
     {
-        $coinMarketCap = CurrencyExchange::getInstance('CoinMarketCap');
-
-        $response = $coinMarketCap->getExchangeRate($request->currency);
-
-        return response()->json([
-            'data' => $response,
-        ], 200);
+        try {
+            $coinMarketCap          = CurrencyExchange::getInstance('CoinMarketCap');
+            $currencyExchangeRate   = $coinMarketCap->getExchangeRate($request->currency);
+            return response()->jsonApi([
+                'type'      => 'success',
+                'title'     => 'Get Coin Market Cap',
+                'message'   => "Get Coin Market Cap",
+                'data'      => $currencyExchangeRate
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->jsonApi([
+                'type'      => 'danger',
+                'title'     => 'Get Coin Market Cap',
+                'message'   => $e->getMessage(),
+                'data'      => []
+            ], 400);
+        }
     }
 }
